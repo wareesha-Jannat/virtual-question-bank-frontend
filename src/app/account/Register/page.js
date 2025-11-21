0de58e58ab4a67be9d96c1727bcc4552386aeaba
Wareesha Jannat <wjannat309@gmail.com>
2025-11-21 18:33:21 +0500

    fixing login issue

--- src/app/account/Register/page.js
+++ src/app/account/Register/page.js
@@ -1,12 +1,14 @@
 "use client";
 import styles from "./Register.module.css";
-import Link from "next/link"
+import Link from "next/link";
 import { useForm } from "react-hook-form";
-import {yupResolver} from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import { toast } from "react-toastify";
-import {registerSchema} from '../../validation/schemas.jsx'
+import { registerSchema } from "../../validation/schemas.jsx";
+import { useRouter } from "next/navigation";
 
 export default function Register() {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -15,25 +17,28 @@ export default function Register() {
   } = useForm({
     resolver: yupResolver(registerSchema),
   });
- 
+
   const onSubmit = async (data) => {
-   try{
-    
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users/register`, {
-      method: "POST",
-      headers: { "content-Type": "application/json" },
-      body: JSON.stringify(data),
-    });
-    const result = await res.json();
-    if (res.ok) {
-         toast.success(result.message)
-      reset();
-    } else {
-      toast.error(result.message)
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/register`,
+        {
+          method: "POST",
+          headers: { "content-Type": "application/json" },
+          body: JSON.stringify(data),
+        }
+      );
+      const result = await res.json();
+      if (res.ok) {
+        toast.success(result.message);
+        reset();
+        router.push("/account/Login");
+      } else {
+        toast.error(result.message);
+      }
+    } catch (err) {
+      toast.error("server error, please try again");
     }
-  }catch(err){
-    toast.error("server error, please try again")
-  }
   };
   return (
     <div className={styles.registerContainer}>
@@ -47,35 +52,41 @@ export default function Register() {
             <input
               type="text"
               id="name"
-              {...register('name')}
-              className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+              {...register("name")}
+              className={`form-control ${errors.name ? "is-invalid" : ""}`}
             />
-           {errors.name && <span className="text-danger">{errors.name.message}</span>}
+            {errors.name && (
+              <span className="text-danger">{errors.name.message}</span>
+            )}
           </div>
 
           <div className="mb-3">
             <label htmlFor="email" className="form-label">
-              Email 
+              Email
             </label>
             <input
-               type="email"
-               id="email"
-               {...register('email')}
-               className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-             />
-             {errors.email && <span className="text-danger">{errors.email.message}</span>}
+              type="email"
+              id="email"
+              {...register("email")}
+              className={`form-control ${errors.email ? "is-invalid" : ""}`}
+            />
+            {errors.email && (
+              <span className="text-danger">{errors.email.message}</span>
+            )}
           </div>
           <div className="mb-3">
             <label htmlFor="password" className="form-label">
               Password
             </label>
             <input
-               type="password"
-               id="password"
-               {...register('password')}
-               className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-             />
-             {errors.password && <span className="text-danger">{errors.password.message}</span>}
+              type="password"
+              id="password"
+              {...register("password")}
+              className={`form-control ${errors.password ? "is-invalid" : ""}`}
+            />
+            {errors.password && (
+              <span className="text-danger">{errors.password.message}</span>
+            )}
           </div>
 
           <div className="mb-3">
@@ -83,12 +94,18 @@ export default function Register() {
               Confirm password
             </label>
             <input
-               type="password"
-               id="passwordConfirmation"
-               {...register('passwordConfirmation')}
-               className={`form-control ${errors.passwordConfirmation ? 'is-invalid' : ''}`}
-             />
-             {errors.passwordConfirmation && <span className="text-danger">{errors.passwordConfirmation.message}</span>}
+              type="password"
+              id="passwordConfirmation"
+              {...register("passwordConfirmation")}
+              className={`form-control ${
+                errors.passwordConfirmation ? "is-invalid" : ""
+              }`}
+            />
+            {errors.passwordConfirmation && (
+              <span className="text-danger">
+                {errors.passwordConfirmation.message}
+              </span>
+            )}
           </div>
 
           <button type="submit" className="btn btn-primary w-100">
